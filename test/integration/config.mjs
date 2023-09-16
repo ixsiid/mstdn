d11b8773ed2e290ace631fc6767cbb748119ec7f
ixsiid <ixsiid@halzion.net>
2023-09-16 15:43:14 +0900

    GitHub Actions上のDynamo DB localエンドポイントに使い分け

--- test/integration/config.mjs
+++ test/integration/config.mjs
@@ -1,2 +1,7 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: './test/integration/.env', override: true });
+dotenv.config({ path: './test/integration/.env', override: false });
+
+// Dynamo DB localのエンドポイントが
+// ローカル実行時は http://localhost:8000
+// GitHub Actionsでは http://dynamodb:8000
+// と、異なるので、単純に上書きしてはならない。

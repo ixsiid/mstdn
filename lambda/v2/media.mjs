2a4ab889ac4d8c470d624aea65eee09fd44e6fff
ixsiid <ixsiid@halzion.net>
2023-09-18 21:22:38 +0900

    メソッド定義だけ

---
+++ lambda/v2/media.mjs
@@ -0,0 +1,12 @@
+/**
+ * @param {IntegrationEvent} event
+ * @param {Auth} auth
+ * @returns {MethodResponse}
+ */
+export default (event, auth) => {
+	/** @type {MethodResponse} */
+	const res = {
+		statusCode: 501
+	};
+};
+

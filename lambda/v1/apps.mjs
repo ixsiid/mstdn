2fd7407bdf4a1396d0f23c763f526f8385defec1
ixsiid <ixsiid@halzion.net>
2023-09-18 12:03:15 +0900

    for subway tooter

--- lambda/v1/apps.mjs
+++ lambda/v1/apps.mjs
@@ -7,10 +7,12 @@ export default event => {
 	if (!vapid_key) {
 		vapid_key = 'BCk-QqERU0q-CfYZjcuB6lnyyOYfJ2AifKqfeGIm7Z-HiTU5T9eTG5GxVA0_OH5mMlI4UkkDTpaZwozy0TzdZ2M=';
 	}
+
 	return {
 		name: event.body.client_name,
 		vapid_key,
 		client_id,
 		website: event.body.website ?? null,
+		client_secret: null,
 	};
 };

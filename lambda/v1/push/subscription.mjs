4cda7ac712381fa05cb11d7eadbbf7ccce8ccd87
ixsiid <ixsiid@halzion.net>
2023-09-23 17:59:04 +0900

    use promise

--- lambda/v1/push/subscription.mjs
+++ lambda/v1/push/subscription.mjs
@@ -18,82 +18,76 @@ export default async (event, auth) => {
 	const dynamo = new DynamoDB(option);
 
 	if (event.httpMethod === 'GET') {
-		try {
-			const conditions = ['account_id = :account_id'];
-			const condition_values = { ':account_id': auth.account_id };
+		const conditions = ['account_id = :account_id'];
+		const condition_values = { ':account_id': auth.account_id };
 
-			const subscription = await dynamo.query({
-				TableName: dynamodb_subscriptions,
-				Limit: 1,
-				ScanIndexForward: false,
-				ExpressionAttributeValues: marshall(condition_values),
-				KeyConditionExpression: conditions.join(' and '),
-			}).catch(err => {
-				throw 'DynamoDB error: ' + err;
-			}).then(res => {
-				console.debug('DynamoDB response');
-				console.debug(res.Items[0]);
-				console.debug(JSON.stringify(unmarshall(res.Items[0])))
-				return unmarshall(res.Items.pop()).subscription;
-			});
+		return dynamo.query({
+			TableName: dynamodb_subscriptions,
+			Limit: 1,
+			ScanIndexForward: false,
+			ExpressionAttributeValues: marshall(condition_values),
+			KeyConditionExpression: conditions.join(' and '),
+		}).catch(err => {
+			throw 'DynamoDB error: ' + err;
+		}).then(res => {
+			console.debug('DynamoDB response');
+			console.debug(res.Items[0]);
+			console.debug(JSON.stringify(unmarshall(res.Items[0])))
 
+			const subscription = unmarshall(res.Items.pop()).subscription;
 			console.debug(subscription);
-			return {
-				statusCode: 200,
-				heders: { type: 'application/json' },
-				body: JSON.stringify({
-					id: auth.account_id,
-					endpoint: subscription.endpoint,
-					server_key: vapid_key,
-					// アラートは設定していないためすべてtrueで返す
-					alerts: {
-						favourite: true,
-						follow: true,
-						mention: true,
-						poll: true,
-						reblog: true
-					}
-				}),
-			};
-		} catch (err) {
+			return subscription;
+		}).then(subscription => ({
+			statusCode: 200,
+			heders: { type: 'application/json' },
+			body: JSON.stringify({
+				id: auth.account_id,
+				endpoint: subscription.endpoint,
+				server_key: vapid_key,
+				// アラートは設定していないためすべてtrueで返す
+				alerts: {
+					favourite: true,
+					follow: true,
+					mention: true,
+					poll: true,
+					reblog: true
+				}
+			}),
+		})).catch(err => {
 			console.error(err);
 			return { statusCode: 404 };
-		}
+		});
 	}
 
 	if (event.httpMethod === 'POST') {
-		try {
-			await dynamo.putItem({
-				TableName: dynamodb_subscriptions,
-				Item: marshall({
-					id: 0,
-					created_at: new Date().getTime(),
-					account_id: auth.account_id,
-					subscription: event.parsed_body.subscription,
-				}),
-			});
-
-			return {
-				statusCode: 200,
-				headers: { type: 'application/json' },
-				body: JSON.stringify({
-					id: auth.account_id,
-					endpoint: event.parsed_body.subscription.endpoint,
-					// eventで要求されたalertsは用いない。それをストアする手段が今はない
-					alerts: {
-						follow: true,
-						favourite: true,
-						reblog: true,
-						mention: true,
-						poll: true,
-					},
-					server_key: vapid_key,
-				}),
-			};
-		} catch (err) {
+		return dynamo.putItem({
+			TableName: dynamodb_subscriptions,
+			Item: marshall({
+				id: 0,
+				created_at: new Date().getTime(),
+				account_id: auth.account_id,
+				subscription: event.parsed_body.subscription,
+			}),
+		}).then(() => ({
+			statusCode: 200,
+			headers: { type: 'application/json' },
+			body: JSON.stringify({
+				id: auth.account_id,
+				endpoint: event.parsed_body.subscription.endpoint,
+				// eventで要求されたalertsは用いない。それをストアする手段が今はない
+				alerts: {
+					follow: true,
+					favourite: true,
+					reblog: true,
+					mention: true,
+					poll: true,
+				},
+				server_key: vapid_key,
+			}),
+		})).catch(err => {
 			console.error(err);
 			return { statusCode: 501 };
-		}
+		});
 	}
 
 	if (event.httpMethod === 'PUT') { }
